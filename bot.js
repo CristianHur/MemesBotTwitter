069bfc39cf28f25006cc1afa4b991146350834c4
Cristian HS <>
2019-05-25 02:16:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -152,7 +152,7 @@ function enviar() {
 				if (!erro) {
 
 					var params = {
-					    status: "#Meme #Memes | Reddit post: http://sh.st/st/bb6c14a58d222943ff7e9f976095b38d/https://www.reddit.com" + linkpost,
+					    status: "#Meme #Memes \n·Reddit post: http://sh.st/st/bb6c14a58d222943ff7e9f976095b38d/https://www.reddit.com" + linkpost,
 						media_ids: [mediaIdStr]
 					}
 
